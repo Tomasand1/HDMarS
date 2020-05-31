a478b2f56ef0e06fc9e15fd7078fa0cd91fcf626
Tomas <andriu.tomas@gmail.com>
2020-05-31 09:31:59 +0300

    Add application theme

---
+++ HDMarS/src/Themes/Fonts.ts
@@ -0,0 +1,29 @@
+const type = {
+    base: 'Helvetica Neue',
+    bold: 'Helvetica-Bold',
+    medium: 'HelveticaNeue-Medium',
+    light: 'Helvetica-Light',
+    emphasis: 'HelveticaNeue-Italic',
+};
+
+const size = {
+    title1: 28,
+    title2: 20,
+    subhead: 15,
+    headline: 17,
+    body: 17,
+    caption: 12,
+};
+
+const style = {
+    h1: {
+        fontFamily: type.base,
+        fontSize: size.h1,
+    },
+};
+
+export default {
+    type,
+    size,
+    style,
+};
